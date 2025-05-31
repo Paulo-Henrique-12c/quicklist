fbdc6ee3cc1273421c9158c48054523574c400e4
paulo007.sn <paulo007.sn@gmail.com>
2025-05-31 21:43:32 +0000

    correção, ajuste do cursor para pointer no label

--- main.js
+++ main.js
@@ -30,7 +30,8 @@ function showItemsList(){
                 <div class="custom-checkbox">
                     <img src="./assets/checked.svg" alt="checked">
                 </div>
-                <label for="item-${index}" onclick="checkItem('${item.name}')>${item.name}</label>
+                <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
+
                 </div>
                 <button>
                     <img src="./assets/trash-icon.svg" alt="trash icon">
